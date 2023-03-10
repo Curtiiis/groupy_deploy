d9e10ec3b529aa776c3c3b2cf46f37dbfb5dbc46
Curtis <curtis@live.fr>
2023-03-10 18:57:20 +0100

    [DEPLOY]: Add files for deploy on Heroku

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
+  </div>
+</template>
+
+<script>
+//EXPORT
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+@import './scss/main.scss';
+#app {
+  min-width: 320px;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.2s ease;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Signup from '../views/Signup.vue'
+import Login from '../views/Login.vue'
+import Welcome from '../views/Welcome.vue'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Welcome',
+    component: Welcome
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
